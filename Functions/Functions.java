95c3c12a2638eea8a9ae802bd5b23a86395afc3e
Sujal Sharma <118937471+Knowsujal@users.noreply.github.com>
2026-05-01 13:45:37 +0530

    Update Functions.java

--- Functions/Functions.java
+++ Functions/Functions.java
@@ -8,7 +8,9 @@ public class Functions{
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-            String name = sc.next();
-            printMyName(name);//function call
+        System.out.print("Enter your name: ");
+        String name = sc.next();
+        printMyName(name); // function call
+        sc.close();
     }
 }
